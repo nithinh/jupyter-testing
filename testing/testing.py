24831615068e04707a379d72447278a5566c099c
Nithin Haridas <nithin@odinix.com>
2020-04-10 23:40:44 -0400

    Modified to fix gradescope errors

--- testing/testing.py
+++ testing/testing.py
@@ -17,7 +17,7 @@ class StackedTimer(object):
 
     def __exit__(self, type, value, traceback):
         if self.name:
-            print(f"{self.name} completed in {round(self.duration(), 3):.3g}s")
+            print("{} completed in {}s".format(self.name,round(self.duration(), 3):.3g))
 
     def duration(self):
         return time.perf_counter() - self.start_time
@@ -39,21 +39,21 @@ class TestImpl(object):
         self.tests_output = []
         tests_message = []
 
-        test_func_name = f"{func.__name__}_test"
+        test_func_name = "{}_test".format(func.__name__)
         test_func = None
         try:
             locals = inspect.currentframe().f_back.f_locals
             test_func = locals[test_func_name]
         except KeyError:
-            tests_message = [f"Cannot locate test function named {test_func_name}"]
+            tests_message = ["Cannot locate test function named {}".format(test_func_name)]
 
         if test_func:
             test_func(func)
 
         print("\n".join(
-            [f"### TESTING {func.__name__}: PASSED {sum(1 if passed else 0 for passed, _ in self.tests_output)}/{len(self.tests_output)}"] +
-            [f"# {o}" for o in tests_message] +
-            [f"# {n}\t: {o[1]}" for n, o in enumerate(self.tests_output) if not o[0]] +
+            ["### TESTING {}: PASSED {}/{}".format(func.__name__,sum(1 if passed else 0 for passed, _ in self.tests_output),len(self.tests_output))] +
+            ["# {}".format(o) for o in tests_message] +
+            ["# {}\t: {}".format(n,o[1]) for n, o in enumerate(self.tests_output) if not o[0]] +
             ["###", ""]))
 
         self.tests_output = None
@@ -68,14 +68,14 @@ class TestImpl(object):
         if value == reference:
             self.tests_output.append((True, "Passed"))
         else:
-            self.tests_output.append((False, f"Failed: {value} is not equal to {reference}"))
+            self.tests_output.append((False, "Failed: {} is not equal to {}".format(value,reference)))
 
     def true(self, prop):
         self.check_scope()
         if prop:
             self.tests_output.append((True, "Passed"))
         else:
-            self.tests_output.append((False, f"Assertion failed"))
+            self.tests_output.append((False, "Assertion failed"))
 
     def exception(self, lmbd, excpt=Exception):
         self.check_scope()
@@ -85,9 +85,9 @@ class TestImpl(object):
             if isinstance(e, excpt):
                 self.tests_output.append((True, "Passed"))
             else:
-                self.tests_output.append((False, f"Exception type {e}, not subclass of {excpt}"))
+                self.tests_output.append((False, "Exception type {e}, not subclass of {}".format(excpt)))
             return
-        self.tests_output.append((False, f"No exception thrown."))
+        self.tests_output.append((False, "No exception thrown."))
 
     def timed(self, *a, **kw):
         return StackedTimer(*a, **kw)
@@ -99,10 +99,10 @@ class TestImpl(object):
             self.tests_output.append((False, "cannot check array similarity without NumPy!"))
 
         elif np.shape(cmp) != np.shape(ref):
-            self.tests_output.append((False, f"Shapes disagree ({np.shape(cmp)} != {np.shape(ref)})"))
+            self.tests_output.append((False, "Shapes disagree ({} != {})".format(np.shape(cmp),np.shape(ref))))
 
         elif not np.allclose(cmp, ref, rtol=0., atol=tol):
-            self.tests_output.append((False, f"Values are not similar"))
+            self.tests_output.append((False, "Values are not similar"))
 
         else:
             self.tests_output.append((True, "Passed"))
